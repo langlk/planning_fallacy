4e0d3ebf82f0522bfff998a782d6b6d7043acb5e
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-18 17:13:30 -0800

    Update to display events.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root to: 'site#home'
-  post '/signin' => 'google#signin'
-  post '/storeauthcode' => 'google#authorize'
   get '/calendar' => 'site#calendar'
   get '/calendar_auth' => 'site#calendar_auth'
   get '/users/auth/google_oauth2' => 'site#calendar_open'
-  get '/oauth' => 'google#oauth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
