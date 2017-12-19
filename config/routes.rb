76732769ec9a6ee8f84f4342f3780261e51bd426
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-19 13:56:44 -0800

    Add controllers and views for users and sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'site#home'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  delete '/signout' => 'sessions#destroy'
+
   get '/calendar' => 'site#calendar'
   get '/calendar_auth' => 'site#calendar_auth'
   get '/users/auth/google_oauth2' => 'site#calendar_open'
