c6cabe6bb1743284b5474af0b5bebc6db2426684
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-20 10:17:55 -0800

    Add tests for late? and earliness methods.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -17,10 +17,31 @@ describe Account do
     end
 
     it "returns average lateness of accounts checkins otherwise" do
-      checkin1 = FactoryBot.create(:checkin, event_time: Time.now - 5.minutes)
-      checkin2 = FactoryBot.create(:checkin, account: checkin1.account, event_time: Time.now + 5.minutes)
-      checkin3 = FactoryBot.create(:checkin, account: checkin1.account, event_time: Time.now - 15.minutes)
+      checkin1 = FactoryBot.create(:checkin, event_time: Time.now - 5.minute)
+      checkin2 = FactoryBot.create(:checkin, account: checkin1.account, event_time: Time.now + 5.minute)
+      checkin3 = FactoryBot.create(:checkin, account: checkin1.account, event_time: Time.now - 15.minute)
       expect(checkin3.account.lateness).to be_within(0.01.minute).of(5.minute)
+    end
+  end
+
+  describe '#earliness' do
+    it "returns how early an account is on average" do
+      checkin1 = FactoryBot.create(:checkin, event_time: Time.now + 5.minute)
+      checkin2 = FactoryBot.create(:checkin, account: checkin1.account, event_time: Time.now - 5.minute)
+      checkin3 = FactoryBot.create(:checkin, account: checkin1.account, event_time: Time.now + 15.minute)
+      expect(checkin3.account.earliness).to be_within(0.01.minute).of(5.minute)
+    end
+  end
+
+  describe '#late?' do
+    it "returns true if account's average lateness is greater than 0" do
+      checkin1 = FactoryBot.create(:checkin, event_time: Time.now - 5.minute)
+      expect(checkin1.account.late?).to eq(true)
+    end
+
+    it "returns false if account's average lateness is less than 0" do
+      checkin1 = FactoryBot.create(:checkin, event_time: Time.now + 1.minute)
+      expect(checkin1.account.late?).to eq(false)
     end
   end
 end
