88ae92e0a04f79860bd1cc359b80a0f7c58892c9
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-21 10:51:06 -0800

    Add note about testing.

--- app/commands/get_calendar_events.rb
+++ app/commands/get_calendar_events.rb
@@ -2,6 +2,7 @@ class GetCalendarEvents
   prepend SimpleCommand
 
   def initialize(account, min_time, max_time)
+    # parsing the time from a string is primarily meant for testing, since calculating from the current time on each test call does not cooperate well with vcr.
     @min_time = min_time.class == String ?
       Time.parse(min_time).iso8601 : (Time.now + min_time.hour).iso8601
     @max_time = max_time.class == String ?
