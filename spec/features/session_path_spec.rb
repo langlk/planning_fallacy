a4af875eec1c3e278cbbca1d09ebdefcdef4feba
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-21 11:03:23 -0800

    Add test for logout.

--- spec/features/session_path_spec.rb
+++ spec/features/session_path_spec.rb
@@ -16,4 +16,13 @@ describe "the sessions management path" do
     click_button 'Sign in'
     expect(page).to have_content("Incorrect email or password.")
   end
+
+  it "allows user to log out" do
+    visit '/signin'
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+    click_button "Sign in"
+    click_link 'Sign out'
+    expect(page).to have_content 'Sign In'
+  end
 end
